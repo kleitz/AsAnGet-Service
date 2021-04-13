5724e1394e359228ca3d2e7338688204e0cb4ef8
Sumit Panwar <73471395+SumitPanwar12@users.noreply.github.com>
2021-04-13 08:16:36 -0700

    instagram

--- auth/instagram.js
+++ auth/instagram.js
@@ -4,16 +4,17 @@ import {instagram} from 'instagram-node';
 
 const api = instagram();
 
-api.use({
-    client_id: 'YOUR_CLIENT_ID',
-    client_secret: 'YOUR_CLIENT_SECRET'
-});
+// api.use({
+//     client_id: 'YOUR_CLIENT_ID',
+//     client_secret: 'YOUR_CLIENT_SECRET'
+// });
 
 const redirect_uri = process.env.INSTANGRAM_BASE_URL + '/instagram_auth';
 
 export const instagramAuth = async (req, res, next) => {
     try {
         const token = req.body.token;
+        console.log(token);
         const response = await axios.get( 'https://graph.instagram.com/me?fields=id,name,email&access_token={token}');
         
         if(!response) return Promise.reject("invalid token");
