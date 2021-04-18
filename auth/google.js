c2af459f67d34e3b811d2e79749359e9b05005ef
Sumit Panwar <73471395+SumitPanwar12@users.noreply.github.com>
2021-04-18 11:02:21 -0700

    google auth

--- auth/google.js
+++ auth/google.js
@@ -3,7 +3,8 @@ import {GOOGLE} from './constant';
 
 
 import {OAuth2Client} from 'google-auth-library';
-const CLIENT_ID = '26791416393-p3m4g25l4m26081mboqrg6e85hukl6vk.apps.googleusercontent.com';
+const CLIENT_ID = '108694993927-gsqt0jorbvq3dt81kfe2ta2eri6kcj68.apps.googleusercontent.com';
+
 const client = new OAuth2Client(CLIENT_ID);
 
 export const googleAuth = async (req, res, next) => {
