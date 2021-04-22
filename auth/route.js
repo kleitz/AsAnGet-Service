a86b78d6c51bb80e445a4d94cb2bc2d584210faa
gajendrapratap <gajendrapratap84@gmail.com>
2021-04-22 07:50:26 +0530

    added for test

--- auth/route.js
+++ auth/route.js
@@ -13,6 +13,14 @@ router.post('/facebook_auth',facebookAuth);
 router.post('/google_auth',googleAuth);
 router.post('/instagram_auth',instagramAuth);
 
+router.get('/test-get-1',requireAuth, async(req, res, next) => {
+    try {
+        return res.status(200).send("hello1234");
+    } catch (error) {
+        next(error);
+    }
+ });
+
 router.get('/test-get',requireAuth, async(req, res, next) => {
     try {
         const data = await getTest();
